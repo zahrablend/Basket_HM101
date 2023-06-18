daf97bb67d6d40c84a9a3bb80f3602daf7d4a168
zahrablend <zahra@LAPTOP-010BK67M>
2023-06-18 17:05:54 +0300

    Add project files.

---
+++ Basket_HM101/Program.cs
@@ -0,0 +1,18 @@
+﻿    /*
+    Create console application where you imitate shopping basket. 
+    You should be able to add, remove or list items. 
+    Your should know the total of the basket. Use OOP. 
+    Create at least 3 different types of products.
+     */
+
+
+namespace Basket_HM101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
