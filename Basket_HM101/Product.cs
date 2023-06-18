6ac624a5d1dfd686c20c361b5ee86260238dec7b
zahrablend <zahra@LAPTOP-010BK67M>
2023-06-18 17:39:33 +0300

    Create Product base class Create Enum of SkinType

---
+++ Basket_HM101/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket_HM101
+{
+    public abstract class Product
+    {
+        public int Id { get; private set; }
+        public string? Brand { get; set; }
+        public decimal Price { get; private set; }
+        public int Volume { get; set; }
+        public SkinType SkinType { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
---
+++ Basket_HM101/SkinType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket_HM101
+{
+    public enum SkinType
+    {
+        Sensitive,
+        Mature,
+        Normal
+    }
+}
